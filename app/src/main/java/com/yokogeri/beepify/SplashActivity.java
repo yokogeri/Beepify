30520510ddc55cc9ba26e289c68e0ff1c7505eb7
yoko_geri <yok89ogeri@gmail.com>
2015-09-02 17:25:52 +0200

    Added Splash Screen Activity Added Splash Screen Layout Modified Manifest

---
+++ app/src/main/java/com/yokogeri/beepify/SplashActivity.java
@@ -0,0 +1,41 @@
+package com.yokogeri.beepify;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+/**
+ * Created by SFAVARET on 02/09/2015.
+ */
+
+public class SplashActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        // TODO Auto-generated method stub
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splashlayout);
+
+        Thread timerThread = new Thread(){
+            public void run(){
+                try{
+                    sleep(3000);
+                }catch(InterruptedException e){
+                    e.printStackTrace();
+                }finally{
+                    Intent intent = new Intent(SplashActivity.this,MainActivity.class);
+                    startActivity(intent);
+                }
+            }
+        };
+        timerThread.start();
+    }
+
+    @Override
+    protected void onPause() {
+        // TODO Auto-generated method stub
+        super.onPause();
+        finish();
+    }
+
+}
